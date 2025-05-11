ebf1552982b30255e36438b9e6210343c3b523fc
Michael Wilburn <michael-mw@hotmail.com>
2025-05-10 23:11:24 -0300

    using the link component

--- src/components/ui/dashboard/nav-links.tsx
+++ src/components/ui/dashboard/nav-links.tsx
@@ -1,4 +1,6 @@
 import { component$ } from "@builder.io/qwik";
+import { Link } from "@builder.io/qwik-city";
+
 
 import {
   HiUserGroupOutline,
@@ -24,14 +26,14 @@ export const NavLinks = component$(() => {
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          <a
+          <Link
             key={link.name}
             href={link.href}
             class="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
           >
             <LinkIcon class="w-6" />
             <p class="hidden md:block">{link.name}</p>
-          </a>
+          </Link>
         );
       })}
     </>
